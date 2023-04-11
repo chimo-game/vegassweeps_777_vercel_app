b36d24ac8617d2399b9c395e44f4d31d61112b77
tolapao <62795957+XANuN@users.noreply.github.com>
2023-04-11 20:47:26 +0700

    a

--- assets/js/access.js
+++ assets/js/access.js
@@ -2,9 +2,9 @@ document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
     var name = document.getElementById("Username").value;
     var email = document.getElementById("exampleInputEmail1").value;
-    var password = document.getElementById("exampleInputPassword1").value;
+    // var password = document.getElementById("exampleInputPassword1").value;
 
-    if (name !== "" && email !== "" && password !== "") {
+    if (name !== "" && email !== "") {
         CPABuildLock();
     } else {
         alert("Please fill out all required fields.");
